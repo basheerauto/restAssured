e1d61c82b578e40ba34e8600910f60d49520c97f
basheer <digiumdsmd@gmail.com>
2020-12-24 22:23:07 +0530

    Test class

--- src/main/java/Utility/Testjava.java
+++ src/main/java/Utility/Testjava.java
@@ -3,5 +3,6 @@ package Utility;
 public class Testjava {
     public static void main(String[] args) {
         System.out.println("welcome to free now api test");
+        System.out.println("welcome to free now api test");
     }
 }
